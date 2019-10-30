8251b7c2691b5ee710ee105f6e40d6c524c8dbcd
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-10-30 11:33:28 -0600

    cam

--- vectores/vectores/vectores.cpp
+++ vectores/vectores/vectores.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <Windows.h>
-#include<stdio.h>
-#include <string.h>
-#include <conio.h>
-using namespace std;
 int main()
 {
 	int numero[5];//aray o vectores de 5 numeros
@@ -19,6 +13,5 @@ int main()
 	suma = numero[0] + numero[1] + numero[2] + numero[3] + numero[4];
 	cout << "La suma es: " << suma;
 	return 0;
-	
 }
 
