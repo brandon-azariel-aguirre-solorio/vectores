1a0e7bfc00128e897e8977de940548011f7496a9
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-10-30 11:24:13 -0600

    cambios

--- vectores/vectores/vectores.cpp
+++ vectores/vectores/vectores.cpp
@@ -8,12 +8,17 @@
 using namespace std;
 int main()
 {
+	int numero[5];//aray o vectores de 5 numeros
+	int suma;// un entero que guarda la sume
 
-
-
-
-
-
-
+	numero[0] = 200;
+	numero[1] = 150;
+	numero[2] = 100;
+	numero[3] = -50;
+	numero[4] = 300;
+	suma = numero[0] + numero[1] + numero[2] + numero[3] + numero[4];
+	cout << "La suma es: " << suma;
+	return 0;
+	
 }
 
