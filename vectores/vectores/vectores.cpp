d6de0e436d612cf9225c06a232ddea15f58a2469
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-10-30 11:15:51 -0600

    xd

---
+++ vectores/vectores/vectores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <Windows.h>
+#include<stdio.h>
+#include <string.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+
+
+
+
+
+
+
+}
+
